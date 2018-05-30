c49c58cd05f52863526f81d2a0e1043053847c61
martynajanas <35773008+martynajanas@users.noreply.github.com>
2018-05-30 17:49:58 +0200

    Add files via upload

--- ZS1-master/8_petle2.js
+++ ZS1-master/8_petle2.js
@@ -7,16 +7,19 @@ var y = prompt("Podaj wartość końcową");
   document.write('<input type="radio" checked name="sort" id="r">rosnąco');
 document.write('<input type="radio" name="sort" id="m">malejąco'); 
   var text="";
+wyswietl.textContent = text;
 function spr(){
   let text=""
   if (r.checked == true){
     for (var z=x; z<=y; z++){
       text +=z;
+      text += " ";
       //console.log(text);
     }
 }  else if (m.checked == true){
     for (var z=y; z>=x; z--){
       text +=z;
+      text += " ";
       //console.log(text);
     }}
     wyswietl.textContent = text;
