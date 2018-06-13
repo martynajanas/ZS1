c51719802952df53c6f9cc632ea9148e75996421
martynajanas <35773008+martynajanas@users.noreply.github.com>
2018-06-13 17:49:00 +0200

    Add files via upload

---
+++ ZS1-master/systemy liczbowe/haslo.php
@@ -0,0 +1,5 @@
+anuluj.addEventListener('click', anulujHaslo);
+  zapisz.addEventListener('click', zapiszHaslo);
+  update.addEventListener('click', updateHaslo);
+  pass1.addEventListener('input', odblokuj);
+  pass2.addEventListener('input', odblokuj);
